71f536905922b18b0564e131f1acc4dda5b0831a
MissWibbon <wuebben.michelle@yahoo.com>
2019-07-08 00:03:13 -0500

    added values to gems and display them on click and add them to points val

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,44 @@
 var guesses = 0;
 var min = 19;
 var max = 120;
+var valMin = 1;
+var valMax = 12;
 var randomNum = Math.random() * (+max - +min) + +min;
-var crystal;
+var ranNumRound = Math.round(randomNum);
+var randomNumber = document.getElementById("randomNumber").innerHTML = ranNumRound;
+var randomVal1 = Math.random() * (+valMax - +valMin) + +valMin;
+var randomVal2 = Math.random() * (+valMax - +valMin) + +valMin;
+var randomVal3 = Math.random() * (+valMax - +valMin) + +valMin;
+var randomVal4 = Math.random() * (+valMax - +valMin) + +valMin;
+
+var blueVal = Math.round(randomVal1);
+var redVal = Math.round(randomVal2);
+var purpleVal = Math.round(randomVal3);
+var greenVal = Math.round(randomVal4);
+
 var userScore = 0;
 var points = 0;
-var blueCrystal = document.getElementById("blueCrystal");
-var redCrystal = document.getElementById("redCrystal");
-var greenCrystal = document.getElementById("greenCrystal");
-var purpleCrystal = document.getElementById("purpleCrystal");
+var blueCrystal = document.getElementById("blueCrystal").onclick = function (){
+    points = points + blueVal;
+    console.log(points);
+    document.getElementById("score").innerHTML = points;
+
+};    
+var redCrystal = document.getElementById("redCrystal").onclick = function (){
+    points = points + redVal;
+    console.log(points);
+    document.getElementById("score").innerHTML = points;
+
+};    
+var greenCrystal = document.getElementById("greenCrystal").onclick = function (){
+    points = points + greenVal;
+    console.log(points);
+    document.getElementById("score").innerHTML = points;
+
+};    
+var purpleCrystal = document.getElementById("purpleCrystal").onclick = function (){
+    points = points + purpleVal;
+    console.log(points);
+    document.getElementById("score").innerHTML = points;
+
+};    
